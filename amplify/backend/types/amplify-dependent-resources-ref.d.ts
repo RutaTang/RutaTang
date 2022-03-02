b6e871a395d064344e50c1046175d51a5b52fd2c
RutaTang <953259325@qq.com>
2022-03-02 08:50:07 +0800

    update amplify

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++
@@ -1,9 +0,0 @@
-export type AmplifyDependentResourcesAttributes = {
-    "api": {
-        "RutaTang": {
-            "GraphQLAPIKeyOutput": "string",
-            "GraphQLAPIIdOutput": "string",
-            "GraphQLAPIEndpointOutput": "string"
-        }
-    }
-}
