61b86488c63ea273eae7b254b1da226ad4cc07bb
Ruta Tang <rutatangpersonal@gmail.com>
2022-04-18 17:05:48 +0800

    update tab icon

--- components/HighWrap.jsx
+++ components/HighWrap.jsx
@@ -1,9 +1,13 @@
 import { useTheme } from "@nextui-org/react";
+import Head from 'next/head';
 
 const HighWrap = (props) => {
   const { isDark, type } = useTheme();
   return (
     <>
+      <Head>
+        <link rel="shortcut icon" href="/svgs/r.svg" />
+      </Head>
       <style jsx global>{`
         :root {
           background-color: ${isDark ? "black" : "white"};
