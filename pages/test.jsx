7b7931594759884ee2b7c9e84721adb6d17ad772
Ruta Tang <rutatangpersonal@gmail.com>
2022-04-24 20:55:40 +0800

    fix err

--- pages/test.jsx
+++ pages/test.jsx
@@ -2,14 +2,9 @@ import { useEffect } from "react";
 import { Button, Input, Text, useTheme } from "@nextui-org/react";
 import { useTheme as useNextTheme } from 'next-themes'
 
-import { API, graphqlOperation } from "aws-amplify";
-import { listTechStacks } from "../src/graphql/queries";
 
 function Test() {
 	const { setTheme } = useNextTheme();
-  useEffect(() => {
-    API.graphql(graphqlOperation(listTechStacks)).then(console.log);
-  }, []);
   return (
     <div className="dark:bg-red-300 bg-blue-300">
 			<button onClick={()=>{setTheme('light')}}>Change Style</button>
