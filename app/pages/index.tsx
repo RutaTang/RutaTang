72b3de88b5293f6bb9a98e511fc9a55394e0fff2
RutaTang <rutatangpersonal@gmail.com>
2022-09-16 22:55:10 +0800

    fix lint err

--- app/pages/index.tsx
+++ app/pages/index.tsx
@@ -23,7 +23,7 @@ const Home = () => {
     >
       <div className="w-full mt-[10%]">
         <h1 className="text-center text-6xl font-black gradient">
-          Welcome to Ruta's Web
+          Welcome to Ruta&apos;s Web
         </h1>
         <div className="w-full flex-col mt-10 space-y-10">
           <div className="w-[80%] mx-auto text-left md:h-[20vh] h-[40vh]">
@@ -31,9 +31,10 @@ const Home = () => {
               /* Interests content */
               <div className="text-center">
                 <span className="text-2xl">
-                  I am a Full 'Interests' (not only stack) Developer across a
-                  number of fields, including <strong>Data Science</strong>,{" "}
-                  <strong>Web Dev</strong>, <strong>Native App Dev</strong>,{" "}
+                  I am a Full &apos;Interests&apos; (not only stack) Developer
+                  across a number of fields, including{" "}
+                  <strong>Data Science</strong>, <strong>Web Dev</strong>,{" "}
+                  <strong>Native App Dev</strong>,{" "}
                   <strong>Cloud Computing</strong>,
                   <strong> Blockchain Dev</strong>,{" "}
                   <strong>Audio Programming</strong>,{" "}
@@ -64,16 +65,25 @@ const Home = () => {
             {tab === Tab.Concat && (
               /*Concat content*/
               <div className="flex justify-center items-center space-x-10 flex-wrap h-full">
-                <a href="https://github.com/RutaTang" target="_blank">
+                <a
+                  href="https://github.com/RutaTang"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <Github size={40} />
                 </a>
                 <a
                   href="https://www.linkedin.com/in/weizhi-tang-3a5137191/"
                   target="_blank"
+                  rel="noreferrer"
                 >
                   <Linkedin size={40} />
                 </a>
-                <a href="https://twitter.com/TangRuta" target="_blank">
+                <a
+                  href="https://twitter.com/TangRuta"
+                  target="_blank"
+                  rel="noreferrer"
+                >
                   <Twitter size={40} />
                 </a>
                 <a href="mailto:rutatangpersonal@gmail.com">
