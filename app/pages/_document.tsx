f026ad2c60c1689a0a37582be01853c9faed888c
RutaTang <rutatangpersonal@gmail.com>
2022-09-17 17:40:03 +0800

    add google tag for google analysis

--- app/pages/_document.tsx
+++ app/pages/_document.tsx
@@ -5,10 +5,27 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          {/* code block syntax highlighting style */}
           <link
             rel="stylesheet"
             href="https://unpkg.com/dracula-prism/dist/css/dracula-prism.css"
           ></link>
+          {/* google analysis tag*/}
+          <script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-PPH3M52YPX"
+          ></script>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-PPH3M52YPX');
+`,
+            }}
+          />
         </Head>
         <body>
           <Main />
